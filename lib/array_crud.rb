98059991504e4f0f4e5804351099c082a61a8432
Theodore Kimana <kimana.theodore@gmail.com>
2019-02-07 22:00:11 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,40 @@
 def create_an_empty_array
-  
-end
+ []
+  end
 
 def create_an_array
+  ["Theodore","james","people","Alex"]
   
-end
+  end
 
 def add_element_to_end_of_array(array, element)
-  
-end
+  << "OK"
+  end
 
 def add_element_to_start_of_array(array, element)
+  element.unshift "Greg"
   
 end
 
 def remove_element_from_end_of_array(array)
+  array.pop "Ok"
   
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array.shift "Greg"
 end
 
 def retrieve_element_from_index(array, index_number)
+  index_number[1]
   
 end
 
 def retrieve_first_element_from_array(array)
+  array [0]
   
 end
 
 def retrieve_last_element_from_array(array)
-  
+  array [-1]
 end
